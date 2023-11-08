524ad923aad94aafe4df7f498897898adf1acf73
kebabzter <gabzterthesecond@gmail.com>
2023-11-08 14:05:22 +0200

    Removed React template files and created 2 test components

---
+++ client/src/components/ButtonComponent/ButtonComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './ButtonComponent.css'
+
+const ButtonComponent = () => {
+    return(
+        <button class="homeButton" >PRESS ME</button>
+    )
+}
+
+export default ButtonComponent;
---
+++ client/src/components/HomeComponent/HomeComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ButtonComponent from '../ButtonComponent/ButtonComponent';
+import './HomeComponent.css'
+
+const HomeComponent = () => {
+    return (
+      <div>
+        <h1>Welcome to the site!</h1>
+        <ButtonComponent></ButtonComponent>
+      </div>
+    );
+}
+
+export default HomeComponent;
