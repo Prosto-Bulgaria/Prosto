a6982190d8f0fca1619a714c168c1cc425c81812
kebabzter <gabzterthesecond@gmail.com>
2023-11-08 14:13:57 +0200

    Removed unnecessary logic
    The logic was regarding a projectService which is not related to the current application. It is to be removed from the template too.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,5 @@
 const authController = require('express').Router();
 const { body, validationResult } = require('express-validator');
-const { getByUserId } = require('../services/projectService');
 
 const { register, login, logout, getUserByUsername } = require('../services/userService');
 const { parseError } = require('../util/parser');
@@ -49,13 +48,5 @@ authController.get('/logout', async (req, res) => {
     res.status(204).end();
 })
 
-authController.get('/profile/:id', async (req, res) => {
-    const username = req.params.id;
-    const currentUser = await getUserByUsername(username);
-    const projects = await getByUserId(currentUser._id);
-    console.log(projects);
-    res.status(200).json(projects);
-    res.end();
-})
 
 module.exports = authController;
