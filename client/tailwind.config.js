9ec633448eeb7f54a6a28af2976093d29928a600
kebabzter <gabzterthesecond@gmail.com>
2024-04-19 22:47:03 +0300

    Changed the color theme to a more saturated better one

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-        colors:{
-            text: 'hsl(228, 38%, 5%)',
-            background: 'hsl(235, 40%, 94%)',
-            primary: 'hsl(233, 41%, 26%)',
-            secondary: 'hsl(5, 41%, 66%)',
-            accent: 'hsl(47, 41%, 48%)',
-            text_dark: 'hsl(228, 38%, 95%)',
-            background_dark: 'hsl(235, 40%, 6%)',
-            primary_dark: 'hsl(233, 41%, 74%)',
-            secondary_dark: 'hsl(5, 41%, 34%)',
-            accent_dark: 'hsl(47, 41%, 52%)',
-        }
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                text: "hsl(198, 56%, 8%)",
+                background: "hsl(197, 47%, 97%)",
+                primary: "hsl(202, 57%, 25%)",
+                secondary: "hsl(2, 57%, 66%)",
+                accent: "hsl(60, 57%, 44%)",
+
+                text_dark: "hsl(198, 56%, 92%)",
+                background_dark: "hsl(197, 47%, 3%)",
+                primary_dark: "hsl(202, 57%, 75%)",
+                secondary_dark: "hsl(2, 57%, 34%)",
+                accent_dark: "hsl(60, 57%, 56%)",
+            },
+        },
     },
-  },
-  plugins: [],
-  darkMode: "class"
+    plugins: [],
+    darkMode: "class",
 };
