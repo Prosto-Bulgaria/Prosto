a1bf686a13dd6bc0c2dcc2fa640af0e85c52cee1
kebabzter <gabzterthesecond@gmail.com>
2024-04-21 00:11:42 +0300

    Scrollbar is now working where needed and matches theme
    After 5 tedious hours, it has been achieved!

--- client/src/components/Main/Theme/Theme.jsx
+++ client/src/components/Main/Theme/Theme.jsx
@@ -31,7 +31,7 @@ const Theme = ({ children }) => {
 
     let sun = <img className="w-5 invert" src={sunLogo} alt="sun" />;
     return (
-        <div className={`${darkMode && "dark"}`}>
+        <div className={`${darkMode? "dark" : "light"}`}>
             <button
                 className="absolute top-[22px] right-14 drop-shadow-md bg-gray-300 dark:bg-white/5 rounded-full p-2"
                 onClick={changeThemeHandler}
--- client/src/hooks/useLocalStorageThemes.js
+++ client/src/hooks/useLocalStorageThemes.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const useLocalStorageThemes = (key, initialValue) => {
     const [state, setState] = useState(() => {
         try{let item = localStorage.getItem(key);
+        document.documentElement.style.setProperty('color-scheme', `${item? "dark" : "light"}`)
 
         return item==="true" ? item : initialValue;
         }catch(err){
@@ -14,6 +15,7 @@ const useLocalStorageThemes = (key, initialValue) => {
         try {
             localStorage.setItem(key, value);
             setState(value);
+            document.documentElement.style.setProperty('color-scheme', `${value? "dark" : "light"}`)
         } catch (err) {
             console.log(err);
         }
