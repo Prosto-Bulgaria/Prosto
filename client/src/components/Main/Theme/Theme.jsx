87cc4af653ef46ba58a854c292d1ec31ba465270
kebabzter <gabzterthesecond@gmail.com>
2024-04-21 00:26:18 +0300

    Got rid of commented code and made theme button non-user-selectable

--- client/src/components/Main/Theme/Theme.jsx
+++ client/src/components/Main/Theme/Theme.jsx
@@ -4,22 +4,6 @@ import sunLogo from "../../../assets/sun.png";
 import useLocalStorageThemes from "../../../hooks/useLocalStorageThemes";
 
 const Theme = ({ children }) => {
-    // if (
-    //     localStorage.theme === "dark" ||
-    //     (!("theme" in localStorage) &&
-    //         window.matchMedia("(prefers-color-scheme: dark)").matches)
-    // ) {
-    //     document.documentElement.classList.add("dark");
-    // } else {
-    //     document.documentElement.classList.remove("dark");
-    // }
-
-    // // Whenever the user explicitly chooses light mode
-    // localStorage.theme = "light";
-
-    // // Whenever the user explicitly chooses dark mode
-    // localStorage.theme = "dark";
-
 
     const [darkMode, setDarkMode] = useLocalStorageThemes('darkMode', true);
 
@@ -33,7 +17,7 @@ const Theme = ({ children }) => {
     return (
         <div className={`${darkMode? "dark" : "light"}`}>
             <button
-                className="absolute top-[22px] right-14 drop-shadow-md bg-gray-300 dark:bg-white/5 rounded-full p-2"
+                className="absolute top-[22px] right-14 drop-shadow-md select-none bg-gray-300 dark:bg-white/5 rounded-full p-2"
                 onClick={changeThemeHandler}
             >
                 {!darkMode ? moon : sun}
