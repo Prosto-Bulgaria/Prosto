39f9f2b9e98fe6d3956c1a6903e8a88c5ef96716
kebabzter <gabzterthesecond@gmail.com>
2024-04-26 14:38:02 +0300

    Minor changes to theme logic

--- client/src/hooks/useLocalStorageThemes.js
+++ client/src/hooks/useLocalStorageThemes.js
@@ -1,20 +1,15 @@
 import { useState } from "react";
 
-const useLocalStorageThemes = (key, initialValue) => {
+const useLocalStorageThemes = (key) => {
     let rootElement = document.documentElement;
 
     const [state, setState] = useState(() => {
-        try {
-            let item = localStorage.getItem(key);
-            rootElement.style.setProperty(
-                "color-scheme",
-                `${item ? "dark" : "light"}`
-            );
-            return item === "true" ? true : false;
-        } catch (err) {
-            console.log(err);
-            return initialValue;
-        }
+        let item = localStorage.getItem(key);
+        rootElement.style.setProperty(
+            "color-scheme",
+            `${item ? "dark" : "light"}`
+        );
+        return item === "true" ? true : false;
     });
 
     const setItem = (value) => {
